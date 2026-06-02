5eff3b12477e2b3b4a09447da7f4efe3a1bc12a6
Maxim Samsonov <m.samsonov@computer.org>
2026-06-02 15:21:25 +0300

    feat: subscriptions list in category settings

--- src/views/Subscription_CreateView.vue
+++ src/views/Subscription_CreateView.vue
@@ -7,15 +7,37 @@ import SubscriptionSettings from '@/components/Subscription_Settings.vue'
 const props = defineProps({
   accountId: {
     type: String,
-    required: true
+    required: false
+  },
+  categoryId: {
+    type: String,
+    required: false
+  },
+  categoryLocked: {
+    type: Boolean,
+    default: false
+  },
+  categoryTitle: {
+    type: String,
+    required: false,
+    default: ''
   }
 })
-const accountId = parseInt(props.accountId, 10)
+const parsedAccountId = props.accountId ? parseInt(props.accountId, 10) : NaN
+const accountIdValue = Number.isFinite(parsedAccountId) ? parsedAccountId : undefined
+const parsedCategoryId = props.categoryId ? parseInt(props.categoryId, 10) : NaN
+const categoryIdValue = Number.isFinite(parsedCategoryId) ? parsedCategoryId : undefined
 </script>
 
 <template>
   <Suspense>
-    <SubscriptionSettings :register="true" :account-id="accountId" />
+    <SubscriptionSettings
+      :register="true"
+      :account-id="accountIdValue"
+      :category-id="categoryIdValue"
+      :category-locked="categoryLocked"
+      :category-title="categoryTitle"
+    />
     <template #fallback>
       <div class="text-center m-5">
         <span class="spinner-border spinner-border-lg align-center"></span>
